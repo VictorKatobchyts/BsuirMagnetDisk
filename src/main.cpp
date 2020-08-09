2c449809dd27776bb6ab708fe1d84d41cd5bddaa
Ilya Romanovskiy <ilya.romanovskiy.2015@mail.ru>
2020-08-09 18:20:17 +0300

    add

--- src/main.cpp
+++ src/main.cpp
@@ -1,158 +1,115 @@
 #include <Arduino.h>
-const int8_t LedPin=21;
-// Аларм мод
-bool AlarmMode=1;
-// Аларм мод
-
-// Сериал инпут
-String inputString = "";         // a String to hold incoming data
-char inByte = 0;         // incoming serial byte
-bool stringComplete;
-String ResultString="";
-// Сериал инпут
- 
- 
-// NeoPixel Ring simple sketch (c) 2013 Shae Erisson
-// Released under the GPLv3 license to match the rest of the
-// Adafruit NeoPixel library
-
-#include <Adafruit_NeoPixel.h>
-
-// Which pin on the Arduino is connected to the NeoPixels?
-#define PIN 5 // Data pin ws2812
-
-// How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS 1 // Popular NeoPixel ring size
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-#define DELAYVAL 500 // Time (in milliseconds) to pause between pixels
-volatile bool activate = 0;// След если в течении минуты было вращение колеса
- 
- // Скорость чтение оборотов в сек (герцы)
- unsigned long timingZamerOborotKoleso; // 
- volatile unsigned long timingLightOtchet; // Переменная которая считает время от последнего срабатывания холла для выключения подсветки ws2812
- volatile unsigned long StopWheelOtchet;// Переменная которая считает время от последнего срабатывания холла для сбрасывания переменных замера первого и второго времени в ноль
- volatile uint32_t count; // Кол-во общее срабатываний
- // Скорость чтение оборотов в сек (герцы)
-
- // Скорость чтение оборотов в сек (герцы)
- void detectsMagnet() {
-     timingLightOtchet = millis();// Присваивание времени когда в последний раз крутилось колесо
-     StopWheelOtchet = millis(); // Таймер считает сколько времени не крутилось колесо. Отсюда берётся точка отсчёта
-     count++; //Прибавка счётчика пронёсшихся мимо магнитов
-     activate=1; // Флаг для ждать мин и погасить
-    
-     //Serial.println("MOTION DETECTED!!!"); 
-}
-// Скорость чтение оборотов в сек (герцы)
-
-void setup() {
-    // put your setup code here, to run once:
-    //pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
-    //pixels.setPixelColor(0, pixels.Color(1, 1, 1));  delay(100);
-    //pixels.show(); delay(100);
-    Serial.begin(115200);
-    while(!Serial){
-    }
-    delay(50);
-
-    //pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
-    pinMode(27,OUTPUT); // Red Lamp
-    pinMode(26,OUTPUT); // Green Lamp
-    pinMode(25,OUTPUT); // Пищалка
-    digitalWrite(27, 1);digitalWrite(26, 1);digitalWrite(25, 1); //Выкл все реле
-    delay(50);
-    // Подвсетка
-    pinMode(LedPin,OUTPUT);
-    digitalWrite(LedPin,LOW);
-
-    delay(100);
-        // Скорость чтение оборотов в сек (герцы)
-    pinMode(16, INPUT_PULLDOWN);
-    attachInterrupt(digitalPinToInterrupt(16), detectsMagnet, RISING);
-    // Скорость чтение оборотов в сек (герцы)
-    Serial.println(1);  Serial.println(1);  Serial.println(1);  Serial.println(1);  Serial.println(1);  Serial.println(1);
-}
 void SerialInput();
+// Для замера времени между проходами магнита
 uint32_t Ftime;
 uint32_t Secondtime;
 bool step=0; // Буль чтоб один раз присваивалось значение времени
 bool PazUnPa=0;
-void loop() {
+unsigned long StopWheelOtchet;// Переменная которая считает время от последнего срабатывания холла для сбрасывания переменных замера первого и второго времени в ноль
+// Для замера времени между проходами магнита
 
-// Тут считаем разницу во времени между 2мя соседними срабатываниями
-  //Eсли число не чётное то присвоить значение текущего времени переменной Ftime 
-  if(count%2==0){
-    if(step==0){
-      Ftime=millis();
-      step=1;
-      PazUnPa=!PazUnPa;
+// Debounce + count
+#define buttonPin  16    // the number of the pushbutton pin
+// Variables will change:
+bool ledState = HIGH;         // the current state of the output pin
+bool buttonState;             // the current reading from the input pin
+bool lastButtonState = LOW;   // the previous reading from the input pin
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+#define debounceDelay  1    // the debounce time; increase if the output flickers
+uint32_t count;
+// Debounce + count
+
+const int8_t LedPin=21;
+bool activate = 0;// Следим если в течении минуты было вращение колеса
+unsigned long timingLightOtchet; // Переменная которая считает время от последнего срабатывания холла для выключения подсветки диод
+
+// Сериал инпут
+//String inputString = "";         // a String to hold incoming data
+char inByte = 0;         // incoming serial byte
+bool stringComplete;
+String ResultString="";
+// Сериал инпут
+void setup() {
+  Serial.begin(115200);
+  pinMode(buttonPin, INPUT);
+  
+    pinMode(27,OUTPUT); // Red Lamp
+    pinMode(26,OUTPUT); // Green Lamp
+    pinMode(25,OUTPUT); // Пищалка
+    digitalWrite(27, 1);digitalWrite(26, 1);digitalWrite(25, 1); //Выкл все реле
+
+    // Подвсетка
+    pinMode(LedPin,OUTPUT);
+    digitalWrite(LedPin,LOW);
+    // Подвсетка
+    //delay(50);
+}
+
+void loop() {
+  //======================= Debounce
+  // read the state of the switch into a local variable:
+  bool reading = digitalRead(buttonPin);
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading != buttonState) {
+      buttonState = reading;
+      if (buttonState == HIGH) {
+        ledState = !ledState;
+        count++;
+        StopWheelOtchet = millis(); // Таймер считает сколько времени не крутилось колесо. Отсюда берётся точка отсчёта
+        activate=1; // Флаг для ждать мин и погасить
+        //Serial.print("count:"); Serial.print(count ); Serial.print(" digitalPinToInterrupt(16):"); Serial.println(digitalRead(16) );
+      }
+    }
+  }
+  lastButtonState = reading;
+  //======================= Debounce
+
+  //Eсли число не чётное то присвоить значение текущего времени переменной Ftime
+  if (count % 2 == 0) {
+    if (step == 0) {
+      Ftime = millis();
+      step = 1;
+      PazUnPa = !PazUnPa;
     }
   }
   else {
-    if(step==1){
-    Secondtime=millis();
-    step=0;
-    PazUnPa=!PazUnPa;
+    if (step == 1) {
+      Secondtime = millis();
+      step = 0;
+      PazUnPa = !PazUnPa;
     }
   }
   //Затем если число чётное то присвоить StopWheelOtchet
-
-//Serial.print ("Сount magnets: "); Serial.print( count ); 
-//Serial.print ("Ftime: "); Serial.print( Ftime ); 
-//Serial.print (" Secondtime: "); Serial.print( Secondtime );
-if(PazUnPa==0) {
- // Serial.print (" Raznost S-F: "); 
-  Serial.print( Secondtime-Ftime ); 
- //  Serial.print (" StopWheelOtchet: "); Serial.print( StopWheelOtchet );
-}
-else{
- // Serial.print (" Raznost F-S: "); 
-  Serial.print( Ftime-Secondtime ); 
-  // Serial.print (" StopWheelOtchet: "); Serial.print( StopWheelOtchet );
-}
-// Если в течении 5 сек нет изминений то сбросить значение Ftime и Secondtime
-
-
-
-Serial.println();
-// Тут считаем разницу во времени между 2мя соседними срабатываниями
-
- // Скорость чтение оборотов в сек + ws2812(герцы)
-// if (millis() - timingZamerOborotKoleso > 1000){ // Вместо 1000 подставьте нужное вам значение паузы   
-   //    Serial.print ("Сount magnets: "); Serial.print( (float)count/3 );  //Обороты в секунду
-       //Serial.print (" activate:"); Serial.print (activate);// Подставить кол-во магнитов
-      // Serial.print (F(" timingLightOtchet: ")); Serial.print( timingLightOtchet ); 
-       //Serial.println();
-       //count=0; 
-        if(activate==1){  //Если сработал датчик кода (Есть движения колеса)     
-           //pixels.setPixelColor(0, pixels.Color(150, 150, 150));pixels.show(); // Загорется белым
-           digitalWrite(LedPin,HIGH);
-        }
-        //else{    
-        //timingZamerOborotKoleso = millis(); //Присвоить значение переменной
-        //}
-
-        if (millis() - timingLightOtchet > 60000){ // Если прошло 60сек
-         activate=0; // Для ждать мин и погасить
-         //pixels.setPixelColor(0, pixels.Color(0, 0, 0));pixels.show(); // Погасить 
-         digitalWrite(LedPin,LOW);
-         timingLightOtchet = millis();
-        }
-        if (millis() - StopWheelOtchet > 5000){ // Если прошло 5 сек и колесо не крутится обнулить время срабатывания между 2мя магнитами
-          Ftime=0;
-          Secondtime=0;
-         //StopWheelOtchet = millis();
-         //Serial.println("Badum");
-        }
-
-
-
-
-       // timingZamerOborotKoleso=millis(); 
-  //  }
- // Скорость чтение оборотов в сек + ws2812(герцы)
-SerialInput(); 
-
+  if (PazUnPa == 0) {
+    // Serial.print (" Raznost S-F: ");
+    Serial.print( Secondtime - Ftime );   //Serial.print( " Count:" ); Serial.print( count );
+    //  Serial.print (" StopWheelOtchet: "); Serial.print( StopWheelOtchet );
+  }
+  else {
+    // Serial.print (" Raznost F-S: ");
+    Serial.print( Ftime - Secondtime );  //Serial.print( " Count:" ); Serial.print( count );
+    // Serial.print (" StopWheelOtchet: "); Serial.print( StopWheelOtchet );
+  }
+  Serial.println();
+  if (millis() - StopWheelOtchet > 5000){ // Если прошло 5 сек и колесо не крутится обнулить время срабатывания между 2мя магнитами
+    Ftime=0;
+    Secondtime=0;     
+  }
+  if(activate==1){  //Если сработал датчик кода (Есть движения колеса)         
+      digitalWrite(LedPin,HIGH);// Загорется белым
+  }
+   if (millis() - timingLightOtchet > 60000){ // Если прошло 60сек
+      activate=0; // Для ждать мин и погасить
+      digitalWrite(LedPin,LOW);// Погасить 
+      timingLightOtchet = millis();
+   }
+   SerialInput();
 }
 
 void SerialInput(){
@@ -160,30 +117,22 @@ void SerialInput(){
  if (Serial.available() > 0) {
     // get incoming byte:
     inByte = (char)Serial.read();
-    inputString += inByte;
+//    inputString += inByte;
+    // Serial.print("inByte:");Serial.println(inByte);
     //Serial.print(inByte);
     if (inByte == '\n')    {   stringComplete = true;    }
  }
-  if (stringComplete)
-  {
-    //Serial.println(inputString);
-     ResultString= inputString;//"L11Z";//inputString;
-    // clear the string:
-    inputString = "";
-    stringComplete = false;
 
-    
-    Serial.print("ResultString:");Serial.println(ResultString);
-    String ResultString2=ResultString.substring(0,1);
-    if(ResultString2=="g")   {  Serial.println(F("Green Lamp"));   digitalWrite( 27, HIGH); digitalWrite( 26, LOW);   }
-    if(ResultString2=="r")   {  Serial.println(F("Red Lamp"));     digitalWrite( 27, LOW) ; digitalWrite( 26, HIGH);  }
-    if(ResultString2=="p")   {  Serial.println("Pisk On");         digitalWrite( 25, LOW) ;  }
-    if(ResultString2=="o")   {  Serial.println("Pisk Off");        digitalWrite( 25, HIGH);  }
+    if(inByte=='g')   {  Serial.println("Green Lamp ON");   digitalWrite( 27, LOW);    }
+    if(inByte=='h')   {  Serial.println("Green Lamp Off");   digitalWrite( 27, HIGH);    }
 
-  }
+    if(inByte=='r')   {  Serial.println("Red Lamp ON");        digitalWrite( 26, LOW);  }
+    if(inByte=='t')   {  Serial.println("Red Lamp Off");       digitalWrite( 26, HIGH);  }
+
+    if(inByte=='p')   {  Serial.println("Pisk On");         digitalWrite( 25, LOW) ;  }
+    if(inByte=='o')   {  Serial.println("Pisk Off");        digitalWrite( 25, HIGH);  }
 // Сериал инпут
 
 
 }
-
 
